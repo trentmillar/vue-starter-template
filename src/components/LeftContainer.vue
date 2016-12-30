dd22d982920188d747761b4dcdfceeb1bbcebe42
trent <trent.millar@gmail.com>
2016-12-30 15:41:07 -0700

    move to laptop

---
+++ src/components/LeftContainer.vue
@@ -0,0 +1,57 @@
+<template lang="pug">
+  div(todo-left-component).leftContent
+        .categoriesContainer
+          .title Categories
+          ul.categories
+            a(href="/this") This
+            a(href="/that") That
+            a(href="/the") The
+            a(href="/other") Other
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="stylus">
+  .leftContent
+    width 250px
+    float left
+    height 100%
+    box-sizing border-box
+    padding-right 0
+    .categoriesContainer
+      width 190px
+      border-radius 2px
+      border 1px solid #ccc
+      margin-top 79px
+      padding-bottom 15px
+      .title
+        margin-top 20px
+        margin-bottom 20px
+        margin-left 20px
+        font-size 14px
+        font-weight 600
+        font-style normal
+        font-stretch normal
+        color #b2b2b2
+        text-transform uppercase
+      ul.categories
+        list-style-type none
+        margin 0
+        padding 0
+        width 150px
+        margin-left 20px
+        a
+          display block
+          text-decoration none
+          cursor pointer
+          font-size 15px
+          font-weight 400
+          font-style normal
+          font-stretch normal
+          line-height 1.8
+          color #4c4c4c
+</style>
---
+++ src/components/PageContent.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  div(todo-page-content-component).pageContent
+    left-content
+    right-content
+</template>
+
+<script>
+import LeftContainer from "./LeftContainer.vue"
+import RightContainer from "./RightContainer.vue"
+
+export default {
+  components: {
+    LeftContent: LeftContainer,
+    RightContent: RightContainer
+  }
+}
+</script>
+
+<style lang="stylus">
+  .pageContent
+    padding-top 92px
+    width 1178px
+    margin auto
+</style>
